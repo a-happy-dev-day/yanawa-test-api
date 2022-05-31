f40ffca554ce64ad4e6541a84be930a08ce9e9fa
tis <92219795+this-is-spear@users.noreply.github.com>
2022-05-31 14:55:31 +0900

    feat : 테스트 api 서버 구성

---
+++ yanawa/src/test/java/fashionable/simba/yanawa/YanawaApplicationTests.java
@@ -0,0 +1,13 @@
+package fashionable.simba.yanawa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YanawaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
