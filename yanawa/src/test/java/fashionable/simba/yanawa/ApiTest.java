935e9ace3ae66660f7bef4af70cb4df3fba50d41
JT LIM <wjdxortaek@gmail.com>
2022-06-06 16:30:07 +0900

    feat: dto 작성 완료 및 테스트

--- yanawa/src/test/java/fashionable/simba/yanawa/ApiTest.java
+++ yanawa/src/test/java/fashionable/simba/yanawa/ApiTest.java
@@ -7,7 +7,7 @@ import org.junit.jupiter.api.Test;
 
 public class ApiTest {
     @Test
-    void Dto_생성테스트() {
+    void Dto_테스트() {
         Long matchingId = 0L;
         MatchingDto dto = new MatchingDto.Builder(matchingId).courtId(0L).build();
         Assertions.assertEquals(dto.getCourtId(),0L);
